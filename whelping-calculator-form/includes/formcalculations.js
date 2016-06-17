b81738bc2f95881ca1f8b943d2c01b6736837e05
Kristen <kristen.e.cahill@gmail.com>
2016-06-17 10:25:56 -0400

    Working on css updates.

--- whelping-calculator-form/includes/formcalculations.js
+++ whelping-calculator-form/includes/formcalculations.js
@@ -1,16 +1,26 @@
+function codeAddress() {
+
+  var today = moment().format('YYYY-MM-DD');
+  $('#myDate').val(today);
+
+  alert('ok');
+}
+window.onload = codeAddress;
+
 function myFunction() {
-    var x =     document.getElementById("myDate").value;
-    document.getElementById("demo").innerHTML = x;
-  
- var popcorn = new Date(x);
-var numberOfDaysToAdd = 63;
-popcorn.setDate(popcorn.getDate() + numberOfDaysToAdd); 
-  
+
+  var x = document.getElementById("myDate").value;
+  document.getElementById("demo").innerHTML = x;
+
+  var popcorn = new Date(x);
+  var numberOfDaysToAdd = 63;
+  popcorn.setDate(popcorn.getDate() + numberOfDaysToAdd);
+
   var dd = popcorn.getDate();
-var mm = popcorn.getMonth() + 1;
-var y = popcorn.getFullYear();
-  
-var someFormattedDate = mm + '/'+ dd + '/'+ y;
-   document.getElementById("demo").innerHTML = someFormattedDate;
-  
+  var mm = popcorn.getMonth() + 1;
+  var y = popcorn.getFullYear();
+
+  var formattedDate = mm + '/' + dd + '/' + y;
+  document.getElementById("demo").innerHTML = formattedDate;
+
 }
